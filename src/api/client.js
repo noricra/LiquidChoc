981c6dc12d818839687328a731abf256b4718659
Noricra <noricra@mac.lan>
2026-02-13 23:13:34 -0500

    fix: add axios interceptors for auth

--- src/api/client.js
+++ src/api/client.js
@@ -5,4 +5,27 @@ const client = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 
+// Interceptor pour ajouter token
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// Interceptor pour détecter 401
+client.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default client
